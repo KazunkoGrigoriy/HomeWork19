0fd76682a76eb1d6e510e38133a5b9cb3812017d
Grigoriy Kazunko <gkazunko@mail.ru>
2022-01-11 22:55:41 +0300

    Домашняя работа номер19

---
+++ HomeWork19/IGeneral.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork19
+{
+    public interface IGeneral
+    {
+        string type { get; set; }
+
+        string breed { get; set; }
+
+        string habitat { get; set; }
+    }
+}
